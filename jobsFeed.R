be61b9f33fd7ddfbc47f7a0d228b8e404feb455d
michael.stedler <m_stedler@gmx.de>
2017-11-22 12:19:07 +0100

    JobsFeed to CSV

--- jobsFeed.R
+++ jobsFeed.R
@@ -9,6 +9,9 @@ doc = htmlParse(html, asText=TRUE)
 
 len = length(xpathSApply(doc, "//item", xmlValue))
 
+
+author = c()
+date = c()
 title = c()
 category = c()
 location = c()
@@ -19,22 +22,28 @@ for(i in 1:len){
   v1 = xpathSApply(doc, paste(item,"title",sep=""), xmlValue)
   v2 = xpathSApply(doc, paste(item,"category",sep=""), xmlValue)
   v3 = xpathSApply(doc, paste(item,"location",sep=""), xmlValue)
+  v4 = xpathSApply(doc, paste(item,"author/name",sep=""), xmlValue)
+  v5 = xpathSApply(doc, paste(item,"updated",sep=""), xmlValue)
   
   # skip tuple values if there is no location or category provided
   if (length(v2)==0 | length(v3)==0 ){
     v1 = "null"
     v2 = "null"
     v3 = "null"
+    v4 = "null"
+    v5 = "null"
   }
   # concatenate all category vaulues to one string
   else{
     v2 = paste(v2, collapse = ';')
   }
+  author[i] = v4
+  date[i] = v5
   title[i] = v1
   category[i] = v2
   location[i] = v3
 }
 
-df = data.frame(title,category,location)
-
+df = data.frame(author,date,title,category,location)
+write.csv(df, file = "jobsFeed.csv")
 
